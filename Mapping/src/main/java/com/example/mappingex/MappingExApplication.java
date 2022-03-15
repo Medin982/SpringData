04cda323b7b7d88186262bfbbe4b7065d4e0feb7
Медин Таракчиев <warss982@gmail.com>
2022-03-15 19:15:17 +0200

    Mapping task

---
+++ Mapping/src/main/java/com/example/mappingex/MappingExApplication.java
@@ -0,0 +1,13 @@
+package com.example.mappingex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MappingExApplication.class, args);
+    }
+
+}
